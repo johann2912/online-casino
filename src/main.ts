a77fc8e4ae7086759442b2d1faad64ae9a17d1c7
Johann Gomez C <johanngomez02@email.com>
2022-05-07 23:49:41 -0500

    :sparkles:/:hammer:  [MODIFY] basic project configuration

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,22 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import * as basicAuth from 'express-basic-auth';
+import { LoggerService } from './config/logger/logger.service';
+import { AllExceptionFilter } from './config/filter';
+import { TimeoutInterceptor } from './config/interceptors/timeout';
+import { ResponseInterceptor } from './config/interceptors/response';
+import { LoggingInterceptor } from './config/interceptors/logger';
 
 async function bootstrap() {
   const configService = new ConfigService();
+  const logger = new LoggerService();
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(
+    new LoggingInterceptor(logger),
+    new ResponseInterceptor(),
+    new TimeoutInterceptor(),
+  );
+  app.useGlobalFilters(new AllExceptionFilter(logger));
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   app.setGlobalPrefix('api/v1/casino/');
