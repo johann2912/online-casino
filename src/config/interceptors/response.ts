66f2aea91dfa8ccfa946367c38ce810a1a47a33a
Johann Gomez C <johanngomez02@email.com>
2022-05-07 23:32:15 -0500

    :sparkles:/:hammer: [CREATE] interceptors and dto responses

---
+++ src/config/interceptors/response.ts
@@ -0,0 +1,34 @@
+import {
+    Injectable,
+    NestInterceptor,
+    ExecutionContext,
+    CallHandler,
+  } from '@nestjs/common';
+  import { Observable } from 'rxjs';
+  import { map } from 'rxjs/operators';
+  import { ResponseFormat } from 'src/lib/dto/responses/format';
+  
+  @Injectable()
+  export class ResponseInterceptor<T>
+    implements NestInterceptor<T, ResponseFormat<T>>
+  {
+    intercept(
+      context: ExecutionContext,
+      next: CallHandler,
+    ): Observable<ResponseFormat<T>> {
+      const now = Date.now();
+      const httpContext = context.switchToHttp();
+      const request = httpContext.getRequest();
+      const response = httpContext.getResponse();
+  
+      return next.handle().pipe(
+        map((data) => ({
+          data,
+          path: request.path,
+          duration: `${Date.now() - now}ms`,
+          method: request.method,
+          code: response.statusCode,
+        })),
+      );
+    };
+};
