5156a8a8143a99a34ad0cd66640e296a05e91549
Johann Gomez C <johanngomez02@email.com>
2022-05-08 20:14:54 -0500

    :sparkles:/:hammer: [CREATE] docker-compose (database local)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { PgDatabaseModule } from './frameworks/database/pg/pg-data.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal:true,
     }),
+    PgDatabaseModule,
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
