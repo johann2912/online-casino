ce744069bc98913bb8d99f184843debea8e24c5c
Johann Gomez C <johanngomez02@email.com>
2022-05-10 10:00:07 -0500

    :sparkles:/:hammer: [MODIFY] all columns nulleables:true in all entities

--- src/frameworks/database/pg/entities/user.entity.ts
+++ src/frameworks/database/pg/entities/user.entity.ts
@@ -5,17 +5,17 @@ import { Timestamp } from "./timestamp.entity";
 @Entity()
 export class User extends Timestamp {
     @PrimaryGeneratedColumn('uuid')
-    id: string;
-    @Column({type: String})
-    nickname: string;
-    @Column({type: String, unique: true})
-    email: string;
-    @Column({type: String})
-    password: string;
-    @Column({type: String})
-    phone: string;
-    @Column({type: Number})
-    credits: number;
-    @Column({type: 'enum', enum: Roles})
-    role: Roles;
+    id?: string;
+    @Column({type: String, nullable:true})
+    nickname?: string;
+    @Column({type: String, nullable:true, unique:true})
+    email?: string;
+    @Column({type: String, nullable:true})
+    password?: string;
+    @Column({type: String, nullable:true})
+    phone?: string;
+    @Column({type: Number, nullable:true})
+    credits?: number;
+    @Column({type: 'enum', enum: Roles, nullable:true})
+    role?: Roles;
 };
