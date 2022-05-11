e647c88daa69a94d0a116346bb11fe06ba1514d9
Johann Gomez C <jgomez@lya-electronic.com>
2022-05-11 16:33:05 -0500

    :sparkles: [VERSIONED] stable version - v1.0.0

--- src/config/swagger/swagger.ts
+++ src/config/swagger/swagger.ts
@@ -10,7 +10,7 @@ export class SwaggerConfig {
         `In this service we will be able to make bets in the game of roulette for an online casino.
         We will be able to buy credits to be able to bet.`,
       )
-      .setVersion('v0.0.0')
+      .setVersion('v1.0.0')
       .build();
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api/v1/casino/docs', app, document, {
