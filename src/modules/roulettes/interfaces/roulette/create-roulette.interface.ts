8c56b4eea42bc2acdf906075f1ff115fe3592d26
Johann Gomez C <johanngomez02@email.com>
2022-05-10 20:29:38 -0500

    :hammer:/:wrench: [MODIFY] create and delete (complete), add row status roulette

--- src/modules/roulettes/interfaces/roulette/create-roulette.interface.ts
+++ src/modules/roulettes/interfaces/roulette/create-roulette.interface.ts
@@ -1,8 +1,10 @@
+import { StatusRoulette } from "src/lib/enum/status-roulette.num";
 import { IRoulette } from "./roulette.interface";
 
 export interface IRouletteCreate extends IRoulette {
     id?:string;
-    name?: string;
-    min_bet?: number;
-    max_bet?: number;
+    name?:string;
+    min_bet?:number;
+    max_bet?:number;
+    status?:StatusRoulette;
 };
--- src/modules/roulettes/output/roulette/create-roulette-output.ts
+++ src/modules/roulettes/output/roulette/create-roulette-output.ts
@@ -1,17 +1,21 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Expose } from "class-transformer";
+import { StatusRoulette } from "src/lib/enum/status-roulette.num";
 import { IRouletteCreate } from "../../interfaces/roulette/create-roulette.interface";
 
 export class RouletteCreateOutputDto implements IRouletteCreate {
     @Expose()
-    id?: string;
+    id?:string;
     @ApiProperty()
     @Expose()
-    name?: string;
+    name?:string;
     @ApiProperty()
     @Expose()
-    min_bet?: number;
+    min_bet?:number;
     @ApiProperty()
     @Expose()
-    max_bet?: number;
+    max_bet?:number;
+    @ApiProperty()
+    @Expose()
+    status?:StatusRoulette
 };
